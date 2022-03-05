f79a741e315bfe6de09f16cc8f626f0d36271e5d
Leonardo da Silva Gomes <leonardodasigomes@gmail.com>
2022-03-04 22:09:18 -0300

    Changed css to ts with styled component

--- src/components/Modal/ModalContainer/ModalContainer.tsx
+++ src/components/Modal/ModalContainer/ModalContainer.tsx
@@ -1,31 +1,26 @@
 import { useHistory } from "react-router-dom";
-import { FaTimes } from 'react-icons/fa'
-import './styles.css'
 
-import * as Styles from './styles';
+import * as Styles from "./styles";
 
-const ModalContainer = ({title, children} : any) => {
+const ModalContainer = ({ title, children }: any) => {
   const history = useHistory();
 
   return (
     <Styles.ModalContainer>
-      <div
-        onClick={history.goBack}
-        className="modalDimScreen"
-      />
-      <div className="modalContent">
-        <div className="modalDialog">
-          <h3 className="modalTitleBox">
-            {title}
-            <FaTimes className="modalIconButton" onClick={history.goBack}/>
-          </h3>  
-          <div className="modalContainerContent">
-            {children}
-          </div>
-        </div>
-      </div>
+      <Styles.ModalDimScreen onClick={history.goBack} />
+
+      <Styles.ModalWrapper>
+        <Styles.ModalDialog>
+          <Styles.ModalHeader>
+            <Styles.Title>{title}</Styles.Title>
+            <Styles.CloseButton onClick={history.goBack} />
+          </Styles.ModalHeader>
+
+          <Styles.ModalContent>{children}</Styles.ModalContent>
+        </Styles.ModalDialog>
+      </Styles.ModalWrapper>
     </Styles.ModalContainer>
-  )
-}
+  );
+};
 
 export default ModalContainer;
