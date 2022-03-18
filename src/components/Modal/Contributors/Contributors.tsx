3d9ffff933e4003640afd8bb77d9422a59dcc3bf
Enzo Gabriel <eggqsaraiva@gmail.com>
2022-03-18 20:07:50 -0300

    🎨 Calling contributors file inestead of creating all contributors data in tsx file

--- src/components/Modal/Contributors/Contributors.tsx
+++ src/components/Modal/Contributors/Contributors.tsx
@@ -1,147 +1,13 @@
 import { ModalContainer } from '@components/Modal';
-import viniciusBrei from '@assets/vinicius_brei.jpg';
-import alexSandy from '@assets/alex_sandy.png';
-import mohsenBahrami from '@assets/mohsen_bahrami.png';
-import alinaFlores from '@assets/alina_flores.png';
-import ricardoLimongi from '@assets/ricardo_limongi.png';
-import leonardoGomes from '@assets/leonardo_gomes.jpg';
-import justinAnderson from '@assets/justin_anderson.png';
-import enzoGabriel from '@assets/enzo_gabriel.jpg';
-import valdersonJunior from '@assets/valderson_junior.png';
-import anaLuisa from '@assets/avatar_icon.svg';
-import andreonMagagna from '@assets/andreon_magagna.jpg';
-import carlaNetto from '@assets/carla_netto.jpg';
-import jaianyRocha from '@assets/jaiany_rocha.png';
-import jhenifferLucas from '@assets/jheniffer_lucas.jpg';
-import luisMenegetti from '@assets/luis_meneguetti.jpg';
-import thalesMartins from '@assets/avatar_icon.svg';
-import vilmarBoff from '@assets/vilmar_boff.png';
+
+import {mainContributors, otherContributors} from './contributors';
 
 import * as Styles from './styles';
-
-interface Person {
-	name: string;
-	image: string;
-	function?: string;
-	institution: string;
-	hasImage?: boolean;
-}
-
-const mainContributors: Person[] = [
-	{
-		name: 'Vinicius Brei',
-		image: viniciusBrei,
-		function: 'Lead Researcher',
-		institution: 'UFRGS',
-	},
-	{
-		name: 'Alex Sandy Pentland',
-		image: alexSandy,
-		function: 'Principal Investigator',
-		institution: 'MIT',
-	},
-	{
-		name: 'Mohsen Bahrami',
-		image: mohsenBahrami,
-		function: 'Lead Researcher',
-		institution: 'MIT',
-	},
-	{
-		name: 'Alina Flores',
-		image: alinaFlores,
-		function: 'Lead Researcher',
-		institution: 'UFRGS',
-	},
-	{
-		name: 'Ricardo Limongi',
-		image: ricardoLimongi,
-		function: 'Lead Researcher',
-		institution: 'UFG',
-	},
-	{
-		name: 'Leonardo Gomes',
-		image: leonardoGomes,
-		function: 'Lead Software Developer',
-		institution: 'Universidade de Brasília',
-	},
-	{
-		name: 'Justin Anderson',
-		image: justinAnderson,
-		function: 'Lead Software Developer',
-		institution: 'MIT',
-	},
-];
-
-const otherContributors: Person[] = [
-	{
-		name: 'Enzo Gabriel',
-		image: enzoGabriel,
-		institution: 'Universidade de Brasília',
-		hasImage: true,
-	},
-	{
-		name: 'Valderson Junior',
-		image: valdersonJunior,
-		institution: 'Universidade de Brasília',
-		hasImage: true,
-	},
-	{
-		name: 'Ana Luisa',
-		image: anaLuisa,
-		institution: '',
-		hasImage: false,
-	},
-
-	{
-		name: 'Andreon Magagna',
-		image: andreonMagagna,
-		institution: 'UFRGS',
-		hasImage: true,
-	},
-
-	{
-		name: 'Carla Netto',
-		image: carlaNetto,
-		institution: '',
-		hasImage: true,
-	},
-	{
-		name: 'Jaiany Rocha',
-		image: jaianyRocha,
-		institution: '',
-		hasImage: true,
-	},
-	{
-		name: 'Jheniffer Dany Lucas',
-		image: jhenifferLucas,
-		institution: '',
-		hasImage: true,
-	},
-	{
-		name: 'Luís Eduardo Meneguetti',
-		image: luisMenegetti,
-		institution: '',
-		hasImage: true,
-	},
-	{
-		name: 'Thales Martins',
-		image: thalesMartins,
-		institution: '',
-		hasImage: false,
-	},
-	{
-		name: 'Vilmar Oro Boff',
-		image: vilmarBoff,
-		institution: '',
-		hasImage: true,
-	},
-];
 
 const Contributors = () => {
 	return (
 		<ModalContainer title="Contributors">
-			{mainContributors.map((mainContributor, index) => {
-				return (
+			{mainContributors.map((mainContributor, index) => (
 					<div key={index}>
 						<Styles.contributorImage
 							src={mainContributor.image}
@@ -157,14 +23,14 @@ const Contributors = () => {
 							</Styles.personData>
 						</Styles.personDatas>
 					</div>
-				);
-			})}
+				)
+			)}
 
 			<br></br>
 			<h2>Developers:</h2>
 			<br></br>
-			{otherContributors.map((otherContributor, index) => {
-				return (
+			{otherContributors.map((otherContributor, index) =>
+				(
 					<>
 						<div key={index}>
 							{otherContributor.hasImage ? (
@@ -195,8 +61,8 @@ const Contributors = () => {
 							</>
 						)}
 					</>
-				);
-			})}
+				)
+			)}
 		</ModalContainer>
 	);
 };
