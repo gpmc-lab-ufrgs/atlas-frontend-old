2ab7eec96d40d715e2d9f51b4b708b1777581ebd
Leonardo da Silva Gomes <leonardodasigomes@gmail.com>
2022-05-04 17:48:37 -0300

    :lipstick: Adjust images footer size

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,39 +1,42 @@
 import {
-	developerInstitutionsLogoImages,
-	supporterInstitutionLogoImages,
-} from './institutions';
+  developerInstitutionsLogoImages,
+  supporterInstitutionLogoImages,
+} from "./institutions";
 
-import { Box } from '@mui/material';
+import { Box } from "@mui/material";
 
-import * as Styles from './styles';
+import * as Styles from "./styles";
 
 function Footer() {
-	return (
-		<Styles.FooterContainer>
-			<Box
-				width="60%"
-				display="flex"
-				justifyContent="space-between"
-				alignItems="center"
-			>
-				{developerInstitutionsLogoImages.map((institution) => (
-					<Styles.InstitutionsLogo
-						src={institution.logoImage}
-						alt={`Logo image of ${institution.name}`}
-						size={institution.size}
-					/>
-				))}
-				<Styles.footerTxt>Apoio:</Styles.footerTxt>
-				{supporterInstitutionLogoImages.map((institution) => (
-					<Styles.InstitutionsLogo
-						src={institution.logoImage}
-						alt={`Logo image of ${institution.name}`}
-						size={institution.size}
-					/>
-				))}
-			</Box>
-		</Styles.FooterContainer>
-	);
+  return (
+    <Styles.FooterContainer>
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        paddingRight={5}
+      >
+        {developerInstitutionsLogoImages.map((institution) => (
+          <Styles.InstitutionsLogo
+            src={institution.logoImage}
+            alt={`Logo image of ${institution.name}`}
+            size={institution.size}
+          />
+        ))}
+      </Box>
+
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Styles.FooterText>Apoio:</Styles.FooterText>
+        {supporterInstitutionLogoImages.map((institution) => (
+          <Styles.InstitutionsLogo
+            src={institution.logoImage}
+            alt={`Logo image of ${institution.name}`}
+            size={institution.size}
+          />
+        ))}
+      </Box>
+    </Styles.FooterContainer>
+  );
 }
 
 export default Footer;
--- src/components/Footer/institutions.ts
+++ src/components/Footer/institutions.ts
@@ -1,68 +1,68 @@
-import logoTramontinaNegativo from '@assets/logoTramontinaNegativo.png';
-import logoSabanciNormal from '@assets/logoSabanciNormal.png';
-import logoUfrgsNegative from '@assets/logoUfrgsNegativo.png';
-import logoUfgNegative from '@assets/logoUfgNegativo.png';
-import logoFgvNegative from '@assets/logoNegativaFGV.png';
-import logoMitNegative from '@assets/logoNegativaMIT.png';
-import logoUnb from '@assets/logoUnb.png';
-import logoNCF from '@assets/logoNCF.png';
+import logoTramontinaNegativo from "@assets/logoTramontinaNegativo.png";
+import logoSabanciNormal from "@assets/logoSabanciNormal.png";
+import logoUfrgsNegative from "@assets/logoUfrgsNegativo.png";
+import logoUfgNegative from "@assets/logoUfgNegativo.png";
+import logoFgvNegative from "@assets/logoNegativaFGV.png";
+import logoMitNegative from "@assets/logoNegativaMIT.png";
+import logoUnb from "@assets/logoUnb.png";
+import logoNCF from "@assets/logoNCF.png";
 
 interface DeveloperInstitutions {
-	name: string;
-	logoImage: string;
-	size: number;
+  name: string;
+  logoImage: string;
+  size: number;
 }
 
 interface SupporterInstiturions {
-	name: string;
-	logoImage: string;
-	size: number;
+  name: string;
+  logoImage: string;
+  size: number;
 }
 
 const developerInstitutionsLogoImages: DeveloperInstitutions[] = [
-	{
-		name: 'Fundação Getulio Vargas',
-		logoImage: logoFgvNegative,
-		size: 110,
-	},
-	{
-		name: 'Federal University of Rio Grande do Sul',
-		logoImage: logoUfrgsNegative,
-		size: 35,
-	},
-	{
-		name: 'New College of Florida',
-		logoImage: logoNCF,
-		size: 30,
-	},
-	{
-		name: 'Federal University of Goiás',
-		logoImage: logoUfgNegative,
-		size: 50,
-	},
-	{
-		name: 'Massachusetts Institute of Technology',
-		logoImage: logoMitNegative,
-		size: 50,
-	},
-	{
-		name: 'Federal University of Brasília',
-		logoImage: logoUnb,
-		size: 30,
-	},
-	{
-		name: 'Sabanci University',
-		logoImage: logoSabanciNormal,
-		size: 40,
-	},
+  {
+    name: "Fundação Getulio Vargas",
+    logoImage: logoFgvNegative,
+    size: 80,
+  },
+  {
+    name: "Federal University of Rio Grande do Sul",
+    logoImage: logoUfrgsNegative,
+    size: 25,
+  },
+  {
+    name: "New College of Florida",
+    logoImage: logoNCF,
+    size: 25,
+  },
+  {
+    name: "Federal University of Goiás",
+    logoImage: logoUfgNegative,
+    size: 40,
+  },
+  {
+    name: "Massachusetts Institute of Technology",
+    logoImage: logoMitNegative,
+    size: 40,
+  },
+  {
+    name: "Federal University of Brasília",
+    logoImage: logoUnb,
+    size: 25,
+  },
+  {
+    name: "Sabanci University",
+    logoImage: logoSabanciNormal,
+    size: 35,
+  },
 ];
 
 const supporterInstitutionLogoImages: SupporterInstiturions[] = [
-	{
-		name: 'Tramontina',
-		logoImage: logoTramontinaNegativo,
-		size: 150,
-	},
+  {
+    name: "Tramontina",
+    logoImage: logoTramontinaNegativo,
+    size: 150,
+  },
 ];
 
 export { developerInstitutionsLogoImages };
