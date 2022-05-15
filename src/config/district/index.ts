0ef199e36c30b981bdd41bb8bdc11ee91082fac5
Leonardo da Silva Gomes <leonardodasigomes@gmail.com>
2022-05-15 17:05:49 -0300

    :technologist: Adjust lint error

--- src/config/district/index.ts
+++ src/config/district/index.ts
@@ -1,15 +1,10 @@
-import { MapPropsSectionType } from "@customTypes/mapProps";
+import { MapPropsSectionType } from '@customTypes/mapProps';
 
-import socioeconomicProps from "./socioeconomic";
-import demographicProps from "./demographic";
-import economicProps from "./economic";
-import socialProps from "./social";
+import socioeconomicProps from './socioeconomic';
+import demographicProps from './demographic';
+import economicProps from './economic';
+import socialProps from './social';
 
-const districtProps: MapPropsSectionType[] = [
-  demographicProps,
-  socialProps,
-  socioeconomicProps,
-  economicProps,
-];
+const districtProps: MapPropsSectionType[] = [demographicProps, socialProps, socioeconomicProps, economicProps];
 
 export default districtProps;
--- src/config/district/socioeconomic.ts
+++ src/config/district/socioeconomic.ts
@@ -1,16 +1,15 @@
-import { formatValue } from "@utils/formatValue";
-import { MapPropsSectionType } from "@customTypes/mapProps";
+import { formatValue } from '@utils/formatValue';
+import { MapPropsSectionType } from '@customTypes/mapProps';
 
 const socioeconomicProps: MapPropsSectionType = {
-  title: "Socioeconômico (SE)",
+  title: 'Socioeconômico (SE)',
   content: [
     {
-      label: "GeoSES",
-      title: "Índice socioeconômico",
-      description:
-        "Índice socioeconômico proposto, variando de -1 a +1 (das piores às melhores condições) - GeoSES",
-      format: (e: any) => formatValue(e, "float_2"),
-      type: "none",
+      label: 'GeoSES',
+      title: 'Índice socioeconômico',
+      description: 'Índice socioeconômico proposto, variando de -1 a +1 (das piores às melhores condições) - GeoSES',
+      format: (e: any) => formatValue(e, 'float_2'),
+      type: 'none',
     },
   ],
 };
