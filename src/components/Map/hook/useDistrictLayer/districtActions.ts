4dcc688da590b8500ab7bcafbf2b2e0821b0365b
valdersonjr <valdersonpontes@gmail.com>
2022-06-23 17:37:51 -0300

    :bug: Fix: district hover popup exiting the map

--- src/components/Map/hook/useDistrictLayer/districtActions.ts
+++ src/components/Map/hook/useDistrictLayer/districtActions.ts
@@ -23,11 +23,13 @@ function setFeatureHover(featureID: number, map: mapboxgl.Map, state: boolean) {
 export function addHoverPopup(feature: mapboxgl.EventData, map: mapboxgl.Map) {
   const regionName = feature.features[0]?.properties?.NM_MUN;
 
-  hoveredPopup
-    .trackPointer()
-    .setHTML(`<div style="display: flex;flex-direction: column;"><h5>${regionName}</h5></div>`);
+  if (hoveredId) {
+    hoveredPopup
+      .trackPointer()
+      .setHTML(`<div style="display: flex;flex-direction: column;"><h5>${regionName}</h5></div>`);
 
-  hoveredPopup.addTo(map);
+    hoveredPopup.addTo(map);
+  }
 }
 
 export function addClickPopup(feature: mapboxgl.EventData, map: mapboxgl.Map) {
