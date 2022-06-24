3adff1d04bbc0655390d50b1a538ae43f769ca35
enzoggqs <eggqsaraiva@gmail.com>
2022-06-23 22:18:48 -0300

    🎨 Remove duplicated code

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -7,10 +7,6 @@ interface HeaderProps {
   isSidebarOpen: boolean;
 }
 
-interface IReturnRoute {
-  selectedLayer: string;
-}
-
 export const HeaderContainer = styled.header<HeaderProps>`
   width: ${({ isSidebarOpen }) => (isSidebarOpen ? 'calc(100% - 385px)' : 'calc(100% - 40px)')};
 
@@ -68,39 +64,4 @@ export const HeaderRightSide = styled.div`
   grid-column: 3;
   justify-self: end;
   align-self: center;
-`;
-
-export const ReturnRoute = styled.div<IReturnRoute>`
-  font-size: 20px;
-  white-space: nowrap;
-  margin-left: 50px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: white;
-  cursor: pointer;
-  pointer-events: auto;
-
-  .country {
-    font-weight: ${({ selectedLayer }) => (selectedLayer === 'country' ? 'bold' : 'normal')};
-  }
-
-  .state {
-    font-weight: ${({ selectedLayer }) => (selectedLayer === 'state' ? 'bold' : 'normal')};
-  }
-
-  .district {
-    font-weight: ${({ selectedLayer }) => (selectedLayer === 'district' ? 'bold' : 'normal')};
-  }
-`;
-
-export const ReturnRouteButton = styled.div`
-  border-radius: 50%;
-  width: 20px;
-  height: 20px;
-  background-color: white;
-  margin-right: 10px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
 `;
