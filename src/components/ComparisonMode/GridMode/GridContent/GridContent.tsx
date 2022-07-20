f80e628033274defe2acc7ca601edf38ce579ae3
enzoggqs <eggqsaraiva@gmail.com>
2022-07-20 01:23:24 -0300

    ✨ Handle subdata view on Grid View

--- src/components/ComparisonMode/GridMode/GridContent/GridContent.tsx
+++ src/components/ComparisonMode/GridMode/GridContent/GridContent.tsx
@@ -20,19 +20,47 @@ const GridContent: React.FC<Props> = ({ section, comparison }) => {
     <Collapsible isTitle={'collapsibleTitle'} title={section.title}>
       <Styles.GridContainer>
         {section.content.map((content: MapPropsContentType, id) => (
-          <Styles.Grid key={id}>
-            <Styles.Title>
-              <h2>{content.title}</h2>
-            </Styles.Title>
-            <Styles.GridItem>
-              {comparison.map((district, idx) => (
-                <Styles.ComparisonLabel key={idx}>
-                  <label title={district?.properties.NM_MUN}>{district?.properties.NM_MUN}</label>
-                  <MetricDetails district={district} metric={content} />
-                </Styles.ComparisonLabel>
-              ))}
-            </Styles.GridItem>
-          </Styles.Grid>
+          <>
+            {!content.nestedData ? 
+              (  
+                <Styles.Grid key={id}>
+                  <Styles.Title>
+                    <h2>{content.title}</h2>
+                  </Styles.Title>
+                  <Styles.GridItem>
+                    {comparison.map((district, idx) => (
+                      <Styles.ComparisonLabel key={idx}>
+                        <label title={district?.properties.NM_MUN}>{district?.properties.NM_MUN}</label>
+                        <MetricDetails district={district} metric={content} />
+                      </Styles.ComparisonLabel>
+                    ))}
+                  </Styles.GridItem>
+                </Styles.Grid>
+              ) : (
+                <Collapsible title={content.title}>
+                  <Styles.GridContainer>
+                    <Styles.GridWithChild>
+                      {content.nestedData.map((data, index) => (
+                        <div key={index}>
+                          <Styles.Title>
+                            <h2>{data.title}</h2>
+                          </Styles.Title>
+                          <Styles.GridItem>
+                            {comparison.map((district, idx) => (
+                              <Styles.ComparisonLabel key={idx}>
+                                <label title={district?.properties.NM_MUN}>{district?.properties.NM_MUN}</label>
+                                <MetricDetails district={district} metric={data} />
+                              </Styles.ComparisonLabel>
+                            ))}
+                          </Styles.GridItem>
+                        </div>
+                      ))}
+                    </Styles.GridWithChild>
+                  </Styles.GridContainer>
+                </Collapsible>
+              )
+            }
+          </>
         ))}
       </Styles.GridContainer>
     </Collapsible>
