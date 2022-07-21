e755c07a1c79e082bf882637345f82493738d52c
Paulo Victor <paulovmel@gmail.com>
2022-07-20 21:54:54 -0300

    chore: removes example test file

--- src/__test__/components/Footer/Footer.spec.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Footer from '@components/Footer';
-
-test('renders the landing page', () => {
-  render(<Footer />);
-});
