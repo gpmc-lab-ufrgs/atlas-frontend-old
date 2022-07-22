ebdf7e9104d58b815da6ac033b5da50f9fd837da
enzoggqs <eggqsaraiva@gmail.com>
2022-07-22 13:34:07 -0300

    🚧 Remove left padding on collapsible that are not titles

--- src/components/Collapsible/styles.ts
+++ src/components/Collapsible/styles.ts
@@ -10,6 +10,7 @@ export const CollapsibleContainer = styled.div<ICollapsibleContainer>`
   .Collapsible__trigger  {
     background-color: ${({ isTitle }) => (isTitle ? '#4A7729' : '#f7f7f7')}!important;
     color: ${({ isTitle }) => (isTitle ? '#ffffff' : '#000000')}!important;
+    padding: ${({ isTitle }) => (!isTitle ? '7px 10px 7px 7px' : '7px 10px')}!important;
   }
 
   .Collapsible__trigger:after {
