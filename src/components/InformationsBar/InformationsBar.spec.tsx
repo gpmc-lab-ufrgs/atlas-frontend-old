768952cf57d4710f222db14da0a65a92e9e672f7
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2022-08-04 11:30:26 -0300

    feat: adding new tests and update variable names

--- src/components/InformationsBar/InformationsBar.spec.tsx
+++ src/components/InformationsBar/InformationsBar.spec.tsx
@@ -7,24 +7,36 @@ describe('InformationsBar', () => {
     /* ********** Cloropleth Legend Component  ********** */
     test('Test the title of Choropletic Legend', () => {
         render(<CloroplethLegend />);
-        const populacao = screen.getByText('População') as HTMLElement ;
-        expect(populacao.textContent).toContain('População');
+        const titleText = screen.getByText('População') as HTMLElement;
+        expect(titleText.textContent).toContain('População');
     });
 
     test('Test the contains of Choropletic Legend', () => {
         render(<CloroplethLegend />);
-        const baixa = screen.getByText('Baixa') as HTMLElement;
-        expect(baixa.textContent).toContain('Baixa');
+        const legendText = screen.getByText('Baixa') as HTMLElement;
+        expect(legendText.textContent).toContain('Baixa');
     });
 
     test('Test the contains of Choropletic Legend', () => {
         render(<CloroplethLegend />);
-        const alta = screen.getByText('Alta') as HTMLElement;
-        expect(alta.textContent).toContain('Alta');
+        const legendText = screen.getByText('Alta') as HTMLElement;
+        expect(legendText.textContent).toContain('Alta');
     });
 
     /* ********** Summary Button Component  ********** */
-    test('Test if there is a button learn more', () => {
+    test('Test the contains of Summary Card', () => {
+        render(<SummaryCard />);
+        const titleText = screen.getByText('Atlas Of Opportunity') as HTMLElement;
+        expect(titleText.textContent).toContain('Atlas Of Opportunity');
+    });
+
+    test('Test the contains of Summary Card', () => {
+        render(<SummaryCard />);
+        const summaryText = screen.getByText('O Atlas de Oportunidade é um projeto que tem o objetivo de ajudar na identificação, mensuração e classificação de necessidades humanas não satisfeitas, estimação de potencial de mercado, previsão de demanda e identificação de oportunidades de negócio em larga escala.') as HTMLElement;
+        expect(summaryText.textContent).toContain('O Atlas de Oportunidade é um projeto que tem o objetivo de ajudar na identificação, mensuração e classificação de necessidades humanas não satisfeitas, estimação de potencial de mercado, previsão de demanda e identificação de oportunidades de negócio em larga escala.');
+    });
+
+    test('Test the contains of Summary Card', () => {
         render(<SummaryCard />);
         const buttonElement = screen.getByRole('button') as HTMLElement;
         expect(buttonElement.textContent).toContain('Saiba mais...');
