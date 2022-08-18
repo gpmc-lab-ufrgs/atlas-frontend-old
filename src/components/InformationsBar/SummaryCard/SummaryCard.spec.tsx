5a0843e9ce2e35542e6fad4b82acf6ed298e81bf
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2022-08-17 21:18:04 -0300

    feat: adding summary card style tests
    Co-authored-by: Thiago Luiz <thiago.luiz20000@gmail.com>

--- src/components/InformationsBar/SummaryCard/SummaryCard.spec.tsx
+++ src/components/InformationsBar/SummaryCard/SummaryCard.spec.tsx
@@ -2,13 +2,35 @@ import React from 'react';
 import 'jest-styled-components';
 import { render, screen } from '@testing-library/react';
 import { SummaryCard } from '.';
+import { Container, Title, Text } from './styles'
 
-describe('InformationsBar', () => {
-    /* ********** Summary Button Component  ********** */
+describe('Summary Card', () => {
+    test('Test the Container component style in Summary Card', () => {
+        const { container: container } = render(<Container />);
+        expect(container.firstChild).toHaveStyleRule('top', '150px');
+        expect(container.firstChild).toHaveStyleRule('position', 'fixed');
+        expect(container.firstChild).toHaveStyleRule('font-size', '14px');
+        expect(container.firstChild).toHaveStyleRule('width', '305px');
+        expect(container.firstChild).toHaveStyleRule('display', 'flex');
+        expect(container.firstChild).toHaveStyleRule('flex-direction', 'column');
+        expect(container.firstChild).toHaveStyleRule('border-radius', '5px !important');
+    });
+
     test('Test the contains of Summary Card', () => {
         render(<SummaryCard />);
         const titleText = screen.getByText('Atlas Of Opportunity') as HTMLElement;
         expect(titleText.textContent).toContain('Atlas Of Opportunity');
+    });
+
+    test('Test the Title component style in Choropleth Legend', () => {
+        const { container: title } = render(<Title />);
+        expect(title.firstChild).toHaveStyleRule('margin', '0px');
+    });
+
+    test('Test the Text component style in Choropleth Legend', () => {
+        const { container: text } = render(<Text />);
+        expect(text.firstChild).toHaveStyleRule('line-height', '150%');
+        expect(text.firstChild).toHaveStyleRule('color', '#1f3349');
     });
 
     test('Test the contains of Summary Card', () => {
