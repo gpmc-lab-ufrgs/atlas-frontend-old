ca349e9ef5cec94690e77b87f8ee995f8dfa82ad
micaellagouveia <micaella2212@gmail.com>
2022-08-18 09:55:45 -0300

    feat: add sidebar tests with different contexts

--- src/__test__/components/SideBar/SideBar.spec.tsx
+++ src/__test__/components/SideBar/SideBar.spec.tsx
@@ -31,4 +31,43 @@ describe('Sidebar', () => {
 
         expect(await findByText('Atlas de Oportunidades')).toBeTruthy();
     });
+
+    test('Teste Sidebar with district selected without comparison', async () => {
+        const { findByText } = render(
+            <BrowserRouter>
+                <selectedDistrictsContext.Provider value={districtMock}>
+                    <comparisonContext.Provider value={comparisonMockFalse}>
+                        <sidebarContext.Provider value={sidebarOpenMock}>
+                            <Sidebar isComparisonMode={false} title="Atlas" />
+                        </sidebarContext.Provider>
+                    </comparisonContext.Provider>
+                </selectedDistrictsContext.Provider>
+            </BrowserRouter>
+        );
+
+        expect(await findByText('Atlas')).toBeTruthy();
+        expect(await findByText('Adicionar a comparação')).toBeTruthy();
+        expect(await findByText('Demográfica (D)')).toBeTruthy();
+        expect(await findByText('Número de mortes observadas por causas evitáveis')).toBeTruthy();
+        expect(await findByText('Número de mortes esperadas')).toBeTruthy();
+    });
+
+    test('Teste Sidebar with comparison and district selected', async () => {
+        const { findByText } = render(
+            <BrowserRouter>
+                <selectedDistrictsContext.Provider value={districtMock}>
+                    <comparisonContext.Provider value={comparisonMock}>
+                        <sidebarContext.Provider value={sidebarOpenMock}>
+                            <Sidebar isComparisonMode={false} title="Atlas" />
+                        </sidebarContext.Provider>
+                    </comparisonContext.Provider>
+                </selectedDistrictsContext.Provider>
+            </BrowserRouter>
+        );
+
+        expect(await findByText('Atlas')).toBeTruthy();
+        expect(await findByText('Remover da comparação')).toBeTruthy();
+        expect(await findByText('Comparação')).toBeTruthy();
+        expect(await findByText('Mostrar comparação')).toBeTruthy();
+    });
 });
