17145bfb5bc93b09824eb4c1f945298baeb5eb8d
Mateus <mateusmaiamaia@hotmail.com>
2022-09-22 13:06:37 -0300

    refactor table mode

--- src/components/ComparisonMode/TableMode/styles.ts
+++ src/components/ComparisonMode/TableMode/styles.ts
@@ -4,7 +4,7 @@ export const TablerContainer = styled.div`
   width: calc(100% - 50px);
   height: fit-content;
 
-  padding: 0px 25px;
+  padding: 0px 600px;
   padding-top: 70px;
   padding-bottom: 35px;
 `;
