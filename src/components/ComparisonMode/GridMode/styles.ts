cacc626741779eaa8bd7f8585d842594fb218d83
Mateus <mateusmaiamaia@hotmail.com>
2022-09-22 13:08:33 -0300

    refactor grid mode

--- src/components/ComparisonMode/GridMode/styles.ts
+++ src/components/ComparisonMode/GridMode/styles.ts
@@ -4,7 +4,7 @@ export const GridContainer = styled.div`
   width: calc(100% - 50px);
   height: fit-content;
 
-  padding: 0px 25px;
+  padding: 0px 200px;
   padding-top: 109px;
   padding-bottom: 35px;
 `;
