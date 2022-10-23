080a5c1408136fcd4f797366fbcb7bae6a869d91
Leonardo da Silva Gomes <leonardodasigomes@gmail.com>
2022-10-22 23:58:23 -0300

    :technologist: Adjust comment made in the pr

--- src/components/Sidebar/RegionDetails/DataSection.tsx
+++ src/components/Sidebar/RegionDetails/DataSection.tsx
@@ -11,6 +11,8 @@ import { MapPropsContentType, MapPropsSectionType } from '@customTypes/mapProps'
 
 import { Tooltip } from '@mui/material';
 
+import { CollapsibleContent } from './CollapsibleContent';
+
 import * as Styles from './styles';
 
 const DataSection: React.FC<MapPropsSectionType> = ({ title, content }) => {
@@ -22,82 +24,42 @@ const DataSection: React.FC<MapPropsSectionType> = ({ title, content }) => {
   const hasSelectedDistrict = Boolean(selected);
 
   return (
-    <Collapsible className="nesttt" isTitle={true} title={title}>
+    <Collapsible isTitle={true} title={title}>
       {content.map((props: MapPropsContentType, id) => (
         <Styles.PropsWrapper key={id}>
-          
-          {!props.nestedData ? 
-            (
-              <>
-                <Tooltip title={props.description} arrow>
-                  <Styles.PropsTitle>{props.title}</Styles.PropsTitle>
-                </Tooltip>
-                {comparison.map((district) => (
-                  <Styles.ValueContent key={district.properties.CD_MUN}>
-                    <p>{district.properties.NM_MUN}</p>
-                    <MetricDetails district={district} metric={props} />
-                  </Styles.ValueContent>
-                ))}
-
-                {!isSelectedOnComparison && hasSelectedDistrict && (
-                  <Styles.ValueContent>
-                    <p>{selected?.properties.NM_MUN}</p>
-                    <MetricDetails district={selected} metric={props} />
-                  </Styles.ValueContent>
+          {!props.nestedData ? (
+            <CollapsibleContent props={props} />
+          ) : (
+            <>
+              <Collapsible isTitle={false} title={props.title}>
+                {comparison && (
+                  <>
+                    {props.nestedData?.map((data, index) => (
+                      <div key={index}>
+                        <CollapsibleContent props={props} />
+                      </div>
+                    ))}
+                  </>
                 )}
-              </>
-            ) : 
-            (
-              <>
-                <Collapsible isTitle={false} title={props.title}>
-                  {comparison && (
-                    <>
-                      {props.nestedData?.map((data, index) => (
-                        <div key={index}>
-                          <Tooltip title={data.description} arrow>
-                            <Styles.PropsTitle>{data.title}</Styles.PropsTitle>
-                          </Tooltip>
-                          {comparison.map((district) => (
-                            <div key={district.properties.CD_MUN}>
-                              <Styles.ValueContent>
-                                <p>{district.properties.NM_MUN}</p>
-                                <MetricDetails district={district} metric={data} />
-                              </Styles.ValueContent>
-                            </div>
-                          ))}
-                          {!isSelectedOnComparison && hasSelectedDistrict && (
-                            <div key={index}>
-                              <Styles.ValueContent>
-                                <p>{selected?.properties.NM_MUN}</p>
-                                <MetricDetails district={selected} metric={data} />
-                              </Styles.ValueContent>
-                          </div>
-                          )}
-                        </div>
-                      ))}
-                    </>
-                  )}
 
-                  {!isSelectedOnComparison && !comparison.length && hasSelectedDistrict && (
-                    <>
-                      {props.nestedData.map((data, index) => (
-                        <div key={index}>
-                          <Tooltip title={data.description} arrow>
-                            <Styles.PropsTitle>{data.title}</Styles.PropsTitle>
-                          </Tooltip>
-                          <Styles.ValueContent>
-                            <p>{selected?.properties.NM_MUN}</p>
-                            <MetricDetails district={selected} metric={data} />
-                          </Styles.ValueContent>
-                        </div>
-                      ))}
-                    </>
-                  )}
-                </Collapsible>
-              </>
-            )
-          }
-
+                {!isSelectedOnComparison && !comparison.length && hasSelectedDistrict && (
+                  <>
+                    {props.nestedData.map((data, index) => (
+                      <div key={index}>
+                        <Tooltip title={data.description} arrow>
+                          <Styles.PropsTitle>{data.title}</Styles.PropsTitle>
+                        </Tooltip>
+                        <Styles.ValueContent>
+                          <p>{selected?.properties.NM_MUN}</p>
+                          <MetricDetails district={selected} metric={data} />
+                        </Styles.ValueContent>
+                      </div>
+                    ))}
+                  </>
+                )}
+              </Collapsible>
+            </>
+          )}
         </Styles.PropsWrapper>
       ))}
     </Collapsible>
