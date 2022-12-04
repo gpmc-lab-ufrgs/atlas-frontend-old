de66c866bbbba5338f48675b209b6e26485d7111
Daniel Barcelos <38585724+daniel-bm@users.noreply.github.com>
2022-12-04 13:02:48 -0300

    Apply clean code (#93)
    * refact: lightordark function
    
    * fix colorManipulator light brightness
    
    * adjust hsp commentary

--- src/utils/colorManipulator.ts
+++ src/utils/colorManipulator.ts
@@ -1,3 +1,5 @@
+const LIGHT_BRIGHTNESS_LIMIT = 127.5;
+
 export function getColorFromGradient(min: string, max: string, value: number) {
   const minColor = stringToColor(min);
   const maxColor = stringToColor(max);
@@ -19,9 +21,10 @@ export function getColorFromGradient(min: string, max: string, value: number) {
 export function lightOrDark(color: string) {
   const rgb = stringToColor(color);
 
-  const hsp = Math.sqrt(0.299 * (rgb.red * rgb.red) + 0.587 * (rgb.green * rgb.green) + 0.114 * (rgb.blue * rgb.blue));
+  // Using HSP (Highly Sensitive Perceived Brightness) system to determinate RGB color brightness
+  const colorBrightness = Math.sqrt(0.299 * (rgb.red * rgb.red) + 0.587 * (rgb.green * rgb.green) + 0.114 * (rgb.blue * rgb.blue));
 
-  if (hsp > 127.5) {
+  if (colorBrightness > LIGHT_BRIGHTNESS_LIMIT) {
     return 'black';
   } else {
     return 'white';
