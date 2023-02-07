a57f9fe613420ea135c132dfdb45245a084a6667
Kevin Luis <k.luis360@gmail.com>
2023-02-07 16:29:31 -0300

    refactor: highlighted state context default value improving variable name

--- src/context/state/highlightedContext.tsx
+++ src/context/state/highlightedContext.tsx
@@ -2,14 +2,17 @@ import React, { createContext, useContext, useState } from 'react';
 
 import { State } from '@customTypes/state';
 
-import { DEFAULT_VALUE } from '@hook/useFeature';
-
 export interface StateActions {
   highlighted: State | null;
   setHighlighted: React.Dispatch<React.SetStateAction<any>>;
 }
 
-export const highlightedStatesContext = createContext<StateActions>(DEFAULT_VALUE);
+const DEFAULT_VALUE_HIGHLIGHTED_CONTEXT: StateActions = {
+  highlighted: null,
+  setHighlighted: () => {},
+};
+
+export const highlightedStatesContext = createContext<StateActions>(DEFAULT_VALUE_HIGHLIGHTED_CONTEXT);
 
 export function HighlightedStatesProvider({ children }: any) {
   const [highlighted, setHighlighted] = useState(null);
