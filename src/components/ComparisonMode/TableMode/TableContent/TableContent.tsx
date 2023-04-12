36a860e82675eccdc40138141e9c4ed180ad148f
demori <andredemori@ime.eb.br>
2023-04-12 17:20:09 -0300

    update table content from database

--- src/components/ComparisonMode/TableMode/TableContent/TableContent.tsx
+++ src/components/ComparisonMode/TableMode/TableContent/TableContent.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Collapsible from '@components/Collapsible';
 
@@ -11,43 +11,62 @@ import { District } from '@customTypes/district';
 import * as Styles from './styles';
 
 interface Props {
-  section: MapPropsSectionType;
   comparison: Array<District>;
 }
 
-const TableContent: React.FC<Props> = ({ section, comparison }) => {
+interface DictionaryData {
+  title: string;
+  content: Array<MapPropsContentType>;
+}
+
+const TableContent: React.FC<Props> = ({ comparison }) => {
+  const [dictionaryData, setDictionaryData] = useState<Array<DictionaryData>>([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('http://127.0.0.1:8000/dictionary/dictionary/json/');
+      const data = await response.json();
+      setDictionaryData(data);
+    }
+    fetchData();
+  }, []);
+
   return (
-    <Collapsible isTitle={true} title={section.title}>
-      {section.content.map((content: MapPropsContentType, id) => (
-        <>
-          {!content.nestedData ? (
-            <Styles.Table lineTableNumber={id} key={id}>
-              <Styles.ColumnTitle>{content.description}</Styles.ColumnTitle>
-              {comparison.map((region, idx) => (
-                <Styles.Column gridColumnNumber={idx + 2} key={idx}>
-                  <MetricDetails district={region} metric={content} />
-                </Styles.Column>
-              ))}
-            </Styles.Table>
-          ) : (
-            <Collapsible isTitle={false} title={content.title}>
-              {content.nestedData?.map((data, index) => (
-                <div key={index}>
-                  <Styles.Table lineTableNumber={index} key={index}>
-                    <Styles.ColumnTitle>{data.description}</Styles.ColumnTitle>
-                    {comparison.map((region, idx) => (
-                      <Styles.Column gridColumnNumber={idx + 2} key={idx}>
-                        <MetricDetails district={region} metric={data} />
-                      </Styles.Column>
-                    ))}
-                  </Styles.Table>
-                </div>
-              ))}
-            </Collapsible>
-          )}
-        </>
+    <>
+      {dictionaryData.map((section: DictionaryData) => (
+        <Collapsible isTitle={true} title={section.title} key={section.title}>
+          {section.content.map((content: MapPropsContentType, id) => (
+            <>
+              {!content.nestedData ? (
+                <Styles.Table lineTableNumber={id} key={id}>
+                  <Styles.ColumnTitle>{content.description}</Styles.ColumnTitle>
+                  {comparison.map((region, idx) => (
+                    <Styles.Column gridColumnNumber={idx + 2} key={idx}>
+                      <MetricDetails district={region} metric={content} />
+                    </Styles.Column>
+                  ))}
+                </Styles.Table>
+              ) : (
+                <Collapsible isTitle={false} title={content.title}>
+                  {content.nestedData?.map((data, index) => (
+                    <div key={index}>
+                      <Styles.Table lineTableNumber={index} key={index}>
+                        <Styles.ColumnTitle>{data.description}</Styles.ColumnTitle>
+                        {comparison.map((region, idx) => (
+                          <Styles.Column gridColumnNumber={idx + 2} key={idx}>
+                            <MetricDetails district={region} metric={data} />
+                          </Styles.Column>
+                        ))}
+                      </Styles.Table>
+                    </div>
+                  ))}
+                </Collapsible>
+              )}
+            </>
+          ))}
+        </Collapsible>
       ))}
-    </Collapsible>
+    </>
   );
 };
 
