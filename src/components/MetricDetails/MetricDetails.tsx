9d1a8017674860b6f888279aa197a620e986bf38
demori <andredemori@ime.eb.br>
2023-04-20 12:29:36 -0300

    udpate

--- src/components/MetricDetails/MetricDetails.tsx
+++ src/components/MetricDetails/MetricDetails.tsx
@@ -5,31 +5,33 @@ const MetricDetails = ({ district, metric }: any) => {
   const [geosesData, setGeosesData] = useState(null);
 
   useEffect(() => {
-    fetch('http://54.163.63.53:8001/data/data_city_dicio/json/')
-      .then(response => response.json())
-      .then(data => setGeosesData(data))
-      .catch(error => console.log(error));
-  }, []);
+    if (district && district.properties && district.properties.SIGLA_UF) {
+      fetch(`http://127.0.0.1:8001/data/data_city_dicio/json/?state=${district.properties.SIGLA_UF}`)
+        .then(response => response.json())
+        .then(data => setGeosesData(data))
+        .catch(error => console.log(error));
+    }
+  }, [district]);
 
   const renderSingleMetric = () => {
-  if (!geosesData) {
-    return <div>Loading data...</div>;
-  }
+    if (!geosesData) {
+      return <div>Loading data...</div>;
+    }
 
-  const rawValue = geosesData[district?.properties.CD_MUN][metric.label];
-  const value = typeof metric.format === 'function' ? metric.format(rawValue) : rawValue; //UPDATE
+    const rawValue = geosesData[district?.properties.CD_MUN][metric.label];
+    const value = typeof metric.format === 'function' ? metric.format(rawValue) : rawValue;
 
-  switch (metric.type) {
-    case 'bar':
-      return <Bar rawValue={rawValue} metric={metric} id={district.properties.CD_MUN} />;
-    default:
-      return (
-        <div key={district.properties.CD_MUN}>
-          <data value={rawValue}>{value}</data>
-        </div>
-      );
-  }
-};
+    switch (metric.type) {
+      case 'bar':
+        return <Bar rawValue={rawValue} metric={metric} id={district.properties.CD_MUN} />;
+      default:
+        return (
+          <div key={district.properties.CD_MUN}>
+            <data value={rawValue}>{value}</data>
+          </div>
+        );
+    }
+  };
 
   return <div>{renderSingleMetric()}</div>;
 };
