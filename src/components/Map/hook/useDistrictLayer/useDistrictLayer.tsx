67ba08452d5545c70b1a69cc871753f3f88ae4bb
demori <andredemori@ime.eb.br>
2023-04-22 19:25:44 -0300

    update

--- src/components/Map/hook/useDistrictLayer/useDistrictLayer.tsx
+++ src/components/Map/hook/useDistrictLayer/useDistrictLayer.tsx
@@ -16,10 +16,10 @@ import geojsonAM from '@data/states/AM_Municipios_2020.json';
 import geojsonAP from '@data/states/AP_Municipios_2020.json';
 import geojsonCE from '@data/states/CE_Municipios_2020.json';
 import geojsonDF from '@data/states/DF_Municipios_2020.json';
-import geojsonES from '@data/states/ES_Municipios_2020.json';
+import geojsonES from '@data/states/ES_Municipios_2020_simplified.json';
 import geojsonGO from '@data/states/GO_Municipios_2020.json';
 import geojsonMA from '@data/states/MA_Municipios_2020.json';
-import geojsonMG from '@data/states/MG_Municipios_2020.json';
+import geojsonMG from '@data/states/MG_Municipios_2020_simplified.json';
 import geojsonMS from '@data/states/MS_Municipios_2020.json';
 import geojsonMT from '@data/states/MT_Municipios_2020.json';
 import geojsonPA from '@data/states/PA_Municipios_2020.json';
@@ -56,7 +56,7 @@ const useDistrictLayer = () => {
 
   const allDistricts = {
     type: 'FeatureCollection',
-    features: [...geojsonRS.features, ...geojsonSC.features, ...geojsonPR.features, ...geojsonSP.features, ...geojsonRJ.features],
+    features: [...geojsonRS.features, ...geojsonSC.features, ...geojsonPR.features, ...geojsonSP.features, ...geojsonRJ.features, ...geojsonMG.features, ...geojsonES.features],
   };
 
   function initLayers(reference: mapboxgl.Map) {
