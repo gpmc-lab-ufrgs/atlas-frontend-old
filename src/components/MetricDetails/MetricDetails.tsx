69b647a15c2001e5424b75948cce61b0102565d1
demori <andredemori@ime.eb.br>
2023-04-29 18:43:29 -0300

    MetricDetails Cache implementation

--- src/components/MetricDetails/MetricDetails.tsx
+++ src/components/MetricDetails/MetricDetails.tsx
@@ -5,13 +5,22 @@ const MetricDetails = ({ district, metric }: any) => {
   const [geosesData, setGeosesData] = useState(null);
 
   useEffect(() => {
-    if (district && district.properties && district.properties.SIGLA_UF) {
-      fetch(`http://15.228.145.19:8001/data/data_city_dicio/json/?cd_mun=${district.properties.CD_MUN}`)
-        .then(response => response.json())
-        .then(data => setGeosesData(data))
-        .catch(error => console.log(error));
+    const cachedData = localStorage.getItem(`geosesData_${district?.properties.CD_MUN}_${metric.label}`); // Check if cached data is available in local storage
+
+    if (cachedData) {
+      setGeosesData(JSON.parse(cachedData)); // If cached data is available, use it
+    } else {
+      if (district && district.properties && district.properties.SIGLA_UF) {
+        fetch(`http://15.228.145.19:8001/data/data_city_dicio/json/?cd_mun=${district.properties.CD_MUN}`)
+          .then(response => response.json())
+          .then(data => {
+            setGeosesData(data);
+            localStorage.setItem(`geosesData_${district?.properties.CD_MUN}_${metric.label}`, JSON.stringify(data)); // Cache the fetched data in local storage
+          })
+          .catch(error => console.log(error));
+      }
     }
-  }, [district]);
+  }, [district, metric]);
 
   const renderSingleMetric = () => {
     if (!geosesData) {
@@ -44,63 +53,62 @@ const MetricDetails = ({ district, metric }: any) => {
         );
 
       case 'Int':
-      return (
-        <div key={district.properties.CD_MUN}>
-          <data value={rawValue}>{parseInt(geosesData[district?.properties.CD_MUN][metric.label].value)}</data>
-        </div>
-      );
-      case 'String':
-      return (
-        <div key={district.properties.CD_MUN}>
-          <data value={rawValue}>{geosesData[district?.properties.CD_MUN][metric.label].value}</data>
-        </div>
-      );
-
-      case 'Progress Bar':
-      // Get the value for the progress bar
-      const value = geosesData[district?.properties.CD_MUN][metric.label].value;
-
-      // Calculate the percentage value of the progress bar
-      const percentage = Math.round((value / 100) * 100);
-
-      // Define a style for the progress bar
-      const progressBarStyle = {
-        width: '100px',
-        height: '20px',
-        borderRadius: '3px',
-        backgroundColor: '#ddd',
-        margin: '10px 0'
-      };
-
-      // Define a style for the filled part of the progress bar
-      const progressBarFilledStyle = {
-        width: `${percentage}%`,
-        height: '100%',
-        borderRadius: '3px',
-        backgroundColor: 'green',
-        textAlign: 'center',
-        color: 'black',
-        fontWeight: 'bold',
-        lineHeight: '20px'
-      };
-
-      return (
-        <div key={district.properties.CD_MUN}>
-          <div style={progressBarStyle}>
-            <div style={progressBarFilledStyle}>{value}%</div>
-          </div>
-        </div>
-      );
-      default:
         return (
           <div key={district.properties.CD_MUN}>
-            <data value={rawValue} >{geosesData[district?.properties.CD_MUN][metric.label].value}</data>
+            <data value={rawValue}>{parseInt(geosesData[district?.properties.CD_MUN][metric.label].value)}</data>
           </div>
         );
-    }
-  };
+      case 'String':
+        return (
+          <div key={district.properties.CD_MUN}>
+            <data value={rawValue}>{geosesData[district?.properties.CD_MUN][metric.label].value}</data>
+          </div>
+        );
 
-  return <div>{renderSingleMetric()}</div>;
+      case 'Progress Bar':
+        // Get the value for the progress bar
+        const value = geosesData[district?.properties.CD_MUN][metric.label].value;
+
+        // Calculate the percentage value of the progress bar
+        const percentage = Math.round((value / 100) * 100);
+
+        // Define a style for the progress bar
+        const progressBarStyle = {
+          width: '100px',
+          height: '20px',
+          borderRadius: '3px',
+          backgroundColor: '#ddd',
+          margin: '10px 0'
+        };
+
+        // Define a style for the filled part of the progress bar
+    const progressBarFilledStyle = {
+      width: `${percentage}%`,
+      height: '100%',
+      borderRadius: '3px',
+      backgroundColor: 'green',
+      textAlign: 'center',
+      color: 'black',
+      fontWeight: 'bold',
+      lineHeight: '20px'
+    };
+
+    return (
+      <div key={district.properties.CD_MUN}>
+        <div style={progressBarStyle}>
+          <div style={progressBarFilledStyle}>{value}%</div>
+        </div>
+      </div>
+    );
+  default:
+    return (
+      <div key={district.properties.CD_MUN}>
+        <data value={rawValue} >{geosesData[district?.properties.CD_MUN][metric.label].value}</data>
+      </div>
+    );
+}};
+
+return <div>{renderSingleMetric()}</div>;
 };
 
 export default MetricDetails;
