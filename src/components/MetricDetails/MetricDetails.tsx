3dcfb148af4490ec5b56d912cf0c76fcd85efd6a
demori <andredemori@ime.eb.br>
2023-05-11 14:46:21 -0300

    update

--- src/components/MetricDetails/MetricDetails.tsx
+++ src/components/MetricDetails/MetricDetails.tsx
@@ -48,7 +48,7 @@ const MetricDetails = ({ district, metric }: any) => {
         const displayValue2 = isNaN(parsedValue2) ? '-----' : parsedValue2.toLocaleString('de-DE', { minimumFractionDigits: 3, maximumFractionDigits: 3 });
         return (
           <div key={district?.properties.CD_MUN}>
-            <data value={geosesDataValue2}>{displayValue2} {metric.unit}</data>
+            <data value={geosesDataValue2}>{displayValue2} - {metric.unit}</data>
           </div>
         );
       case 'Float .2 (-1 to +1)':
