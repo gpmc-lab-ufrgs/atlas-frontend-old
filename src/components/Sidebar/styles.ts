3ebddce169dd9dad366551d7d74661b9b35b94d8
demori <andredemori@ime.eb.br>
2023-08-19 21:07:30 -0300

    need to modify sidebar, regiondetails and datasection

--- src/components/Sidebar/styles.ts
+++ src/components/Sidebar/styles.ts
@@ -1,5 +1,35 @@
 import styled from 'styled-components';
 
+import { Link } from 'react-router-dom';
+
+export const ComparisonButton = styled(Link)`
+  display: flex;
+  align-items: center;
+  align-self: center;
+
+  margin-top: 20px;
+  padding: 5px 15px;
+
+  background-color: rgba(0, 64, 111, 0.9);
+
+  text-decoration: none;
+  text-align: center;
+  font-size: 14px;
+  font-weight: 500;
+  color: #fff;
+
+  box-sizing: border-box;
+  border: 1px solid #000;
+  border-radius: 100px;
+
+  cursor: pointer;
+
+  p {
+    margin: 0;
+  }
+`;
+
+
 export const SidebarContent = styled.div`
   width: 305px;
 `;
