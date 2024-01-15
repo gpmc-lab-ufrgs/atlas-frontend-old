d2044d50582d361c5076c586538e23072f0f506d
Jose Ricardo <ricardo.junior_@outlook.com>
2024-01-14 22:37:52 -0300

    ajustes

--- src/api/atlasApi.ts
+++ src/api/atlasApi.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const APIAtlasV2 = axios.create({
-  baseURL: 'http://localhost:5000/',
+  baseURL: 'http://localhost:8080/',
   headers: {
     'Cache-Control': 'no-cache',
     Pragma: 'no-cache',
